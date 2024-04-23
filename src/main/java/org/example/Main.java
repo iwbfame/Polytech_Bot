1dd8a846a84cd0d74cc41771f8d366ed06bc6a2b
Viktor Azarov <arcticque421@gmail.com>
2024-04-23 21:09:24 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+import org.example.Bot.Bot;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        String botToken = "6773697252:AAGHqhqOoLsyI1TYhd9gr6jRekZdIpP_c8M";
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new Bot(botToken));
+            System.out.println("MyAmazingBot successfully started!");
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/org/example/responseCommand/ResponseCommand.java
@@ -0,0 +1,4 @@
+package org.example.responseCommand;
+
+public class ResponseCommand {
+}
